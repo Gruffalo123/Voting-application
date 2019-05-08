3c976c814e5379134726d5d546d2c2da3e681e9d
Gruffalo <houxinxinbox@163.com>
2019-05-08 18:04:07 +0800

    part5 and part6

--- polls/models.py
+++ polls/models.py
@@ -13,7 +13,9 @@ class Question(models.Model):
 
     # 演示
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse,HttpResponseRedirect
 from .models import Question,Choice
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 # Create your views here.
 
 # 通用视图
@@ -14,12 +15,17 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+                Excludes any questions that aren't published yet.
+                """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
